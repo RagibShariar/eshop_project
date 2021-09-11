767475698ab2fff53aa80bcb4b9640cee158d912
RagibShariar <srragib@gmail.com>
2021-09-11 22:16:38 +0600

    first commit

---
+++ models/init.php
@@ -0,0 +1,10 @@
+<?php 
+
+include "models/config.php";
+include "models/controller.php";
+include "models/functions.php";
+include "models/database.php";
+include "models/setting.class.php";
+include "models/page.class.php";
+include "models/search.class.php";
+include "models/app.php";
